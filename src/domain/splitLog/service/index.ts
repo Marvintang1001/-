f92108f76a3a19d9865c83d535b17efad5e90b25
marvintang唐晓明 <marvintang@126.com>
2021-04-20 17:46:47 +0800

    去除测试console

--- src/domain/splitLog/service/index.ts
+++ src/domain/splitLog/service/index.ts
@@ -32,7 +32,6 @@ export class SplitLogService extends AbcSplitLog {
         for (const x of Object.keys(targetObj)) {
             const content = contentObj[x][0];
             if (content?.volume != targetObj[x]) {
-                console.log(content?.volume, targetObj[x][0]?.volume);
                 throw new ApiError(`该物品(${JSON.stringify(content)})拆分前后总数不相等`);
             }
         }
